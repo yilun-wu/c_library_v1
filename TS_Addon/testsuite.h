ae985b472e5061094b7a27f2ed0534ea8ca9984b
Yilun Wu <david.yilun.wu@gmail.com>
2017-06-15 18:36:22 -0400

    commited but will not use v1 for future

---
+++ TS_Addon/testsuite.h
@@ -0,0 +1,37 @@
+/** @file
+ *    @brief MAVLink comm protocol testsuite generated from TS_Addon.xml
+ *    @see http://qgroundcontrol.org/mavlink/
+ */
+#pragma once
+#ifndef TS_ADDON_TESTSUITE_H
+#define TS_ADDON_TESTSUITE_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef MAVLINK_TEST_ALL
+#define MAVLINK_TEST_ALL
+
+static void mavlink_test_TS_Addon(uint8_t, uint8_t, mavlink_message_t *last_msg);
+
+static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+
+    mavlink_test_TS_Addon(system_id, component_id, last_msg);
+}
+#endif
+
+
+
+
+
+static void mavlink_test_TS_Addon(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
+{
+
+}
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+#endif // TS_ADDON_TESTSUITE_H
